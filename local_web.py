516e25b305d01526620b7eeed25e885126469cec
Andy Roberts <andy.roberts@avalara.com>
2017-04-29 20:44:25 -0700

    initial commit

---
+++ local_web.py
@@ -0,0 +1,73 @@
+__author__ = 'krishnateja'
+
+import logging
+import datetime
+from flaskext.mysql import MySQL
+from flask import Flask, render_template
+from flask_ask import Ask, statement, question
+
+mysql = MySQL()
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'conference'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'qJ32wxcN'
+app.config['MYSQL_DATABASE_DB'] = 'conference'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+ask = Ask(app, "/")
+
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
+
+@app.route("/")
+def main():
+    speaker_name = "Justin"
+    cursor = mysql.connect().cursor()
+
+    cursor.execute(
+        "SELECT * from conference.talk JOIN conference.speaker "
+        "ON conference.talk.speaker = conference.speaker.speakerid "
+        "JOIN conference.slot "
+        "ON conference.talk.slot = conference.slot.slotid "
+        "WHERE conference.speaker.name LIKE '%" + speaker_name + "%';")
+
+    data = cursor.fetchall()
+    if data is None:
+        message = "There was no speaker by that name."
+    else:
+        for row in data:
+            message = "I found a session called '%s' by %s at %s" % (row[1], row[6], row[9])
+
+    return message
+
+@ask.launch
+def new_conference():
+    welcome_msg = render_template('welcome')
+    return question(welcome_msg)
+
+
+@ask.intent("SpeakerIntent")
+def ask_if_speaker(speaker_name):
+    speaker_name = str(speaker_name)
+
+    cursor = mysql.connect().cursor()
+
+    cursor.execute(
+        "SELECT * from conference.talk JOIN conference.speaker "
+        "ON conference.talk.speaker = conference.speaker.speakerid "
+        "JOIN conference.slot "
+        "ON conference.talk.slot = conference.slot.slotid "
+        "WHERE conference.speaker.name LIKE '%" + speaker_name + "%';")
+
+    data = cursor.fetchall()
+    if data is None:
+        message = "There was no speaker by that name."
+    else:
+        for row in data:
+            message = "Welcome to Conference Info: I found a session called '%s' by %s at %s" % (row[1], row[6], row[9])
+
+    return statement(message)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080)
